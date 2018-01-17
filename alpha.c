bc1d639a38ecb4507cc9f3ba09f75bb7d666a4e8
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-17 18:55:15 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter any alphabet");
+scanf("%c",&ch);
+if((ch>=65&&ch<=90)!!(ch>=97&&ch<=122))
+{
+printf("entered character is alphabet");
+}
+else
+{
+printf("entered character is alphabet");
+}
+getch();
+}
