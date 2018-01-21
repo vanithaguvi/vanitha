3b4b6e6ee23432c97f96594fd09b4dd2beef39bc
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-21 11:59:58 +0530

    Create prgm21.c

---
+++ prgm21.c
@@ -0,0 +1,51 @@
+
+
+#include<stdio.h>
+	
+	
+
+int main()
+	
+
+{
+	
+
+    int n,a b;
+	
+
+    printf("first term",a);
+	
+
+    scanf("%d",&a);
+	
+
+    printf("common difference",b);
+	
+
+    scanf("%d",&b);
+	
+
+    printf("N term",n);
+	
+
+    scanf("%d",&n);
+	
+
+    while(counter<=n)
+	
+
+    {
+	
+
+         a=1+(counter-1)*b;
+	
+
+         printf("%d",a);
+	
+
+         counter++;
+	
+
+}
+
+return 0;
