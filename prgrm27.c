691e55ee776fa9584f9ce5e9b683a31c1a96adc5
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-21 12:14:58 +0530

    Create prgrm27.c

---
+++ prgrm27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+	void main()
+	{
+	int a;
+	char ch;
+	scanf("%d",&a);
+	if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+	{
+	printf("%d not a number",&a);
+	}
+	else
+	{
+	printf("%d number",&a);
+	}
+	}
