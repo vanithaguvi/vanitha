1d4b030f16ca36727afc02c849f4b353c02da6e3
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-21 10:36:26 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -4,10 +4,13 @@ int main(void)
 int i,c,n,a=1,b=10;
 printf("\n enter the number");
 scanf("%d",&n);
+  if(a<b)
+  {
 for(i=1;i<=n;i++);
 {
 c++;
 }
+  }
 if(c==2)
 {
 printf("\n given no.is a prime no");
