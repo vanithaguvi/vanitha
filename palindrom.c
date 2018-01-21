b74c9d66cb69eb7251145f6bab9ecedf9dfb9930
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-21 09:47:21 +0530

    Rename palindrome.c to palindrom.c

---
+++ palindrom.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,rem,rev=0,p;
+printf("enter the number");
+scanf("%d",&n);
+p=n;
+while(n!=0)
+{
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+if(t==rev)
+{
+printf("%d is palindrome",n);
+}
+else
+{
+printf("%d is not a palindrome",n);
+}
+return 0;
+}
