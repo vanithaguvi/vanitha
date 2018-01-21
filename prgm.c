9a0c80962e1b2fba6f84c5cdb672ce590d2dc0ce
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-21 11:27:19 +0530

    Create prgm.c

---
+++ prgm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+int i=5,n,num=1,c;
+printf("enter the no.");
+scanf("%d",&n);
+while(i<=n)
+{
+c=num*i;
+num++
+printf("result is%d",c);
+}
+return 0;
+]
+
+
+
