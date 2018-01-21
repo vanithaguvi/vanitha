56ebca93713e1a062b4131daa000b8847698d6c6
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-21 11:07:46 +0530

    Create primeone.c

---
+++ primeone.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+int i,c,n;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+c++
+}
+if(c==2)
+{
+printf("prime no.");
+}
+else
+{
+printf("non prime number");
+}
+return 0;
+}
