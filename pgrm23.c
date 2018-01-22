977afdee63736d813c12007a024e0a89ce5bea15
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-22 18:53:27 +0530

    Create pgrm23.c

---
+++ pgrm23.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a[25],min;
+printf("enter the numbers");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+min=a[0];
+}
+printf("minimum number is%d",min);
+return 0;
+}
