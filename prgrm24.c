e22fb478d26753c58f0ccbc093ea2fb4430bb67e
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-24 16:19:43 +0530

    Create prgrm24.c

---
+++ prgrm24.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n,i,mid;
+printf("Enter the size of an array");
+scanf("%d",&n)
+for(i=0;i<=n,i++)
+printf("ENTER THE ARRAY");
+scanf("%d",&a[i])
+{
+if(a[1]!=0)
+}
+{
+if((!a[i]<a[n])&&!a[i]>a[n]))
+{
+a[i]=mid;
+}
+}
+else
+{
+printf("error");
+}
+return 0;
+}
