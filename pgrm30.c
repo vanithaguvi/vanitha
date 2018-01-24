a55edc5e71ad557077741298b5438fbf5838a890
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-01-24 16:38:11 +0530

    Create pgrm30.c

---
+++ pgrm30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+printf("\n enter the first time");
+scanf("%d%d",&a,&b);
+printf("\n enter the second time");
+scanf("%d%d",&c,&d);
+e=a-c;
+f=b-d;
+printf("time is%d\n%d",e,f);
+return 0;
+}
