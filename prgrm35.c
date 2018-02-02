4df1e8e9325b389f3fd7ed57b37053c7d6bfcb7e
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-02-02 16:12:17 +0530

    Create prgrm35.c

---
+++ prgrm35.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,count=0;
+char ch[20];
+scanf("%s",&ch);
+for(i=0;i<=n;i++)
+{
+if((ch[i]>='0')&&(ch[i]<='9'))
+{
+count++
+}
+}
+printf("%d",count);
+return 0;
+}
+
