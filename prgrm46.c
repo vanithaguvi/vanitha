58982de4bd12c4cd51498bda5bfdce182e9e8fdb
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-02-05 16:30:50 +0530

    Create prgrm46.c

---
+++ prgrm46.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the num");
+scanf("%d",&n);
+if(n>0)
+{
+printf("next element is%d",n+1);
+}
+else
+{
+printf("no operation");
+}
+return 0;
+}
