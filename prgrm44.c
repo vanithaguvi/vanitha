7393cd30e394467f4691cae1c5e1664e730fb6e8
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-02-05 16:23:19 +0530

    Create prgrm44.c

---
+++ prgrm44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n>=1)&&(n<=10))
+{
+printf("num is present");
+}
+else
+{
+printf("not present");
+}
+return 0;
+}
