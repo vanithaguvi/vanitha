c7b5f13433c27c059b7110c6f4fb7526aa4766ec
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-02-12 16:57:10 +0530

    Create p53.c

---
+++ p53.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter the number");
+scanf("%d",&n);
+if(a%10!=0)
+{
+b=a/10;
+c=a%10;
+d=a/10;
+e=a%10;
+res=c+d+e;
+}
+printf("result is%d",res);
+return 0;
+}
