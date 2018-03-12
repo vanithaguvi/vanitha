3522b076b5104729629c89b79cdcbd050a79f78e
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-03-12 15:47:28 +0530

    Create p73.c

---
+++ p73.c
@@ -0,0 +1,18 @@
+  #include<stdio.h>
+  int main()
+  {
+  int n,l,r;
+  printf("enter the number");
+  scanf("%d",&n);
+  printf("enter the limits");
+  scanf("%d %d",&l,&r);
+  if(n>l&&n<r)
+  {
+  printf("yes");
+  }
+  else
+  {
+  printf("no");
+  }
+  return 0;
+  }
