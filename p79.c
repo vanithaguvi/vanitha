212fc6a93078d0c70196513566945b0caa7494a5
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-03-14 15:53:59 +0530

    Create p79.c

---
+++ p79.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,m,sq;
+printf("enter the numbers");
+scanf("%d %d",&a,&b);
+m=a*b;
+sq=sqrt(m);
+if((sq==a)&9sq==b))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
