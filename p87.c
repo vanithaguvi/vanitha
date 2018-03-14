3ac0264919625d043bc246393ce4fc66709d468d
vanithaguvi <31506133+vanithaguvi@users.noreply.github.com>
2018-03-14 16:47:39 +0530

    Create p87.c

---
+++ p87.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the numbers:");
+scanf("%d %d",&a,&b);
+if(a==0)
+{
+printf("%d",b);
+}
+else
+{
+while((a!=0)&&(b!=0))
+{
+if(a>b)
+a=a-b;
+else
+b=a+b;
+}
+printf("%d",a);
+}
+return 0;
+}
+
